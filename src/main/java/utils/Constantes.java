155feda0043141d614fef4e79ab88c7bed9c8a68
KasuraS <guo.yefan@hotmail.fr>
2020-11-13 18:07:12 +0100

    new unit tests

--- src/main/java/utils/Constantes.java
+++ src/main/java/utils/Constantes.java
@@ -13,4 +13,19 @@ public class Constantes{
     public static final String UPDATE_UNIQUE = "UPDATE PROGRAMMEUR SET NOM = ?, PRENOM = ?, ADRESSE = ?, PSEUDO = ?, RESPONSABLE = ?, HOBBY = ?, DATE_NAISS = ?, DATE_EMB = ? WHERE MATRICULE = ?";
     public static final String DELETE_UNIQUE = "DELETE FROM PROGRAMMEUR WHERE MATRICULE = ?";
     public static final String INSERT_UNIQUE = "INSERT INTO PROGRAMMEUR (MATRICULE, NOM, PRENOM, ADRESSE, PSEUDO, RESPONSABLE, HOBBY, DATE_NAISS, DATE_EMB) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    // For testing purposes
+    public static final String TESTADDPROG_MATR = "21";
+    public static final String TESTADDPROG_NOM = "hello";
+    public static final String TESTADDPROG_PRENOM = "world";
+    public static final String TESTADDPROG_ADR = "11 rue de Paris";
+    public static final String TESTADDPROG_PSEUDO = "hello-chan";
+    public static final String TESTADDPROG_RESP = "JAA";
+    public static final String TESTADDPROG_HOBBY = "coding";
+    public static final String TESTADDPROG_DATNAISS = "1967-11-11";
+    public static final String TESTADDPROG_DATEMP = "1997-11-11";
+
+    public static final String TESTUPDPROG_PSEUDO = "world-chan";
+    public static final String TESTUPDPROG_RESP = "Cherel";
+    public static final String TESTUPDPROG_DATEMP = "2017-12-13";
 }
--- test/test.java
+++ test/test.java
@@ -1,18 +1,82 @@
 import modele.ActionsBDImpl;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import utils.Constantes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class test {
+    @Mock
+    private Connection dbConn;
 
-    public ActionsBDImpl conn = new ActionsBDImpl();
+    @Mock
+    private PreparedStatement pstmt;
 
-    @Test
-    public void stubbingReturnValue() {
-        Assert.assertTrue(true);
+    @Before
+    public void setUp() {
+        try{
+            dbConn = DriverManager.getConnection(Constantes.URL, Constantes.USER, Constantes.MDP);
+        } catch (SQLException sqle) {
+            Logger.getLogger(ActionsBDImpl.class.getName()).log(Level.SEVERE, null, sqle);
+        }
     }
 
     @Test
-    public void testAddProgrammer() {
-        Assert.assertNotEquals(0, conn.getProgrammeurs().size());
+    public void testAjouterProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(dbConn);
+        pstmt = dbConn.prepareStatement(Constantes.INSERT_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_MATR);
+        pstmt.setString(2, Constantes.TESTADDPROG_NOM);
+        pstmt.setString(3, Constantes.TESTADDPROG_PRENOM);
+        pstmt.setString(4, Constantes.TESTADDPROG_ADR);
+        pstmt.setString(5, Constantes.TESTADDPROG_PSEUDO);
+        pstmt.setString(6, Constantes.TESTADDPROG_RESP);
+        pstmt.setString(7, Constantes.TESTADDPROG_HOBBY);
+        pstmt.setString(8, Constantes.TESTADDPROG_DATNAISS);
+        pstmt.setString(9, Constantes.TESTADDPROG_DATEMP);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertEquals(1, isSuccessful);
+    }
+
+    @Test
+    public void testModifierProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(dbConn);
+        pstmt = dbConn.prepareStatement(Constantes.UPDATE_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_NOM);
+        pstmt.setString(2, Constantes.TESTADDPROG_PRENOM);
+        pstmt.setString(3, Constantes.TESTADDPROG_ADR);
+        pstmt.setString(4, Constantes.TESTUPDPROG_PSEUDO);
+        pstmt.setString(5, Constantes.TESTUPDPROG_RESP);
+        pstmt.setString(6, Constantes.TESTADDPROG_HOBBY);
+        pstmt.setString(7, Constantes.TESTADDPROG_DATNAISS);
+        pstmt.setString(8, Constantes.TESTUPDPROG_DATEMP);
+        pstmt.setString(9, Constantes.TESTADDPROG_MATR);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertNotEquals(0, isSuccessful);
+    }
+
+    @Test
+    public void testSupprimerProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(dbConn);
+        pstmt = dbConn.prepareStatement(Constantes.DELETE_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_MATR);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertNotEquals(0, isSuccessful);
     }
 }
