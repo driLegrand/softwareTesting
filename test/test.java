f52df70dc18e28961311989d08b4bf2328940c51
Théo <t.abdmeziem@gmail.com>
2020-11-15 23:50:41 +0100

    displayAfficherTousTest

--- test/test.java
+++ test/test.java
@@ -1,6 +1,10 @@
 import modele.ActionsBDImpl;
 import org.junit.Assert;
 import org.junit.Test;
+import vue.Vue;
+
+import javax.swing.*;
+import java.lang.reflect.Field;
 
 public class test {
 
@@ -14,5 +18,25 @@ public class test {
     @Test
     public void testAddProgrammer() {
         Assert.assertNotEquals(0, conn.getProgrammeurs().size());
+    }
+
+    @Test
+    public void testDisplayAfficherTous() throws NoSuchFieldException, IllegalAccessException {
+        final Vue ihm = new Vue();
+        ihm.displayAfficherTous();
+
+        final Field field = ihm.getClass().getSuperclass().getDeclaredField("paneAfficher");
+        field.setAccessible(true);
+
+        final Field field2 = ihm.getClass().getSuperclass().getDeclaredField("zoneAffichageProgrammeurs");
+        field2.setAccessible(true);
+
+        JPanel paneAfficherTest = new JPanel();
+        JTextArea zoneAffichageProgrammeursTest = new JTextArea(10, 70);
+        JScrollPane scrollTest = new JScrollPane(zoneAffichageProgrammeursTest);
+        paneAfficherTest.add(scrollTest);
+
+        Assert.assertEquals("paneAfficher didn't match.", paneAfficherTest.toString(), field.get(ihm).toString());
+        Assert.assertEquals("zoneAffichageProgrammeurs didn't match.", zoneAffichageProgrammeursTest.toString(), field2.get(ihm).toString());
     }
 }
