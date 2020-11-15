343cec9166403d8bbf6772e5ec5f613ee5f7e722
Hadrien Tournès <hadrien.tournes@efrei.net>
2020-11-15 19:03:09 +0100

    improved test on displayModifierFinal()

--- test/test.java
+++ test/test.java
@@ -1,11 +1,11 @@
 import modele.ActionsBDImpl;
 import org.junit.Assert;
 import org.junit.Test;
+import vue.Vue;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.lang.reflect.Field;
 
 public class test {
 
@@ -17,61 +17,35 @@ public class test {
     }
 
     @Test
-    public void testAddProgrammer() {
-        Assert.assertNotEquals(0, conn.getProgrammeurs().size());
-    }
+    public void testDisplayModifierFinal() throws NoSuchFieldException, IllegalAccessException {
+        final Vue vue = new Vue();
+        final Field field = vue.getClass().getSuperclass().getDeclaredField("paneGenerique");
+        field.setAccessible(true);
 
-    @Test
-    public void testDisplayModifierFinal() {
-        class FakeVue implements ActionListener {
-            private JButton btnRechercher;
-            private JButton btnReinitialiser;
-            private JButton btnValiderEcranModifier;
-            private JButton btnAnnuler;
+        field.set(vue, new JPanel());
+        vue.displayModifierFinal();
 
-            protected JPanel paneGenerique = new JPanel();
-
-            public void setButtonBounds(JButton button, int x) {
-                button.setBounds(x, 200, 100, 30);
-            }
-
-            public void displayModifierFinal() {
-                btnRechercher = new JButton("Rechercher");
-                btnReinitialiser = new JButton("Réinitialiser");
-                btnValiderEcranModifier = new JButton("Valider");
-                btnAnnuler = new JButton("Annuler");
-                paneGenerique.add(btnRechercher);
-                paneGenerique.add(btnReinitialiser);
-                paneGenerique.add(btnValiderEcranModifier);
-                paneGenerique.add(btnAnnuler);
-                btnRechercher.addActionListener(this);
-                btnValiderEcranModifier.addActionListener(this);
-                btnReinitialiser.setEnabled(false);
-                btnAnnuler.addActionListener(this);
-                setButtonBounds(btnRechercher, 85);
-                setButtonBounds(btnReinitialiser, 190);
-                setButtonBounds(btnValiderEcranModifier, 295);
-                setButtonBounds(btnAnnuler, 400);
-            }
-
-            @Override
-            public void actionPerformed(ActionEvent e) {}
-        }
-        FakeVue fake = new FakeVue();
-        fake.displayModifierFinal();
-        Assert.assertEquals(4, fake.paneGenerique.getComponentCount());
-        Component[] comps = fake.paneGenerique.getComponents();
+        Assert.assertEquals(4, ((JPanel) field.get(vue)).getComponentCount());
+        Component[] comps = ((JPanel) field.get(vue)).getComponents();
         for (Component c : comps) {
             Assert.assertEquals(JButton.class, c.getClass());
             JButton button = (JButton) c;
-            if (button.getText() != "Réinitialiser")
+            if (button.getText() != "Réinitialiser") {
                 Assert.assertEquals(1, button.getActionListeners().length);
-            else
+                Assert.assertTrue(button.isEnabled());
+            } else {
                 Assert.assertEquals(0, button.getActionListeners().length);
+                Assert.assertFalse(button.isEnabled());
+            }
         }
         Assert.assertEquals(new Rectangle(85, 200, 100, 30), comps[0].getBounds());
         Assert.assertEquals(new Rectangle(190, 200, 100, 30), comps[1].getBounds());
         Assert.assertEquals(new Rectangle(295, 200, 100, 30), comps[2].getBounds());
         Assert.assertEquals(new Rectangle(400, 200, 100, 30), comps[3].getBounds());
+    }
+
+    @Test
+    public void testActionPerformed() {
+        
     }
 }
