0aab51e9776a25deefff9a9246975e820f77abb1
Théo <t.abdmeziem@gmail.com>
2020-11-16 11:21:51 +0100

    testDisplaySupprimer

--- test/test.java
+++ test/test.java
@@ -5,6 +5,7 @@ import vue.Vue;
 
 import javax.swing.*;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class test {
 
@@ -67,6 +68,69 @@ public class test {
         ihm.setButtonBounds(btnRechercherTest, 85);
         ihm.setButtonBounds(btnReinitialiserTest, 190);
         ihm.setButtonBounds(btnValiderEcranAjouterTest, 295);
+        ihm.setButtonBounds(btnAnnulerTest, 400);
+
+        Assert.assertEquals("paneGenerique didn't match.", paneGeneriqueTest.toString(), field.get(ihm).toString());
+    }
+
+    @Test
+    public void testDisplaySupprimer() throws NoSuchFieldException, IllegalAccessException {
+        final Vue ihm = new Vue();
+        ihm.display();
+        ihm.displayAjouter();
+
+        final Field field = ihm.getClass().getSuperclass().getDeclaredField("paneGenerique");
+        field.setAccessible(true);
+
+        JPanel paneGeneriqueTest = new JPanel();
+        paneGeneriqueTest.setLayout(null);
+
+        JButton btnRechercherTest = new JButton("Rechercher");
+        JButton btnReinitialiserTest = new JButton("Réinitialiser");
+        JButton btnValiderEcranSupprimerTest = new JButton("Valider");
+        JButton btnAnnulerTest = new JButton("Annuler");
+        paneGeneriqueTest.add(btnRechercherTest);
+        paneGeneriqueTest.add(btnReinitialiserTest);
+        paneGeneriqueTest.add(btnValiderEcranSupprimerTest);
+        paneGeneriqueTest.add(btnAnnulerTest);
+
+        ArrayList<JComponent> listComponentsTest = new ArrayList<>();
+        JLabel labelMatriculeTest = new JLabel("Matricule");
+        JTextField champMatriculeTest = new JTextField();
+
+        listComponentsTest.add(labelMatriculeTest);
+        listComponentsTest.add(champMatriculeTest);
+        listComponentsTest.add(new JLabel("Nom"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Prénom"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Adresse"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Pseudo"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Responsable"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Hobby"));
+        listComponentsTest.add(new JTextField());
+        listComponentsTest.add(new JLabel("Date de naissance"));
+        listComponentsTest.add(new JTextField("jour"));
+        Object[] mois = new Object[]{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
+        listComponentsTest.add(new JComboBox(mois));
+        listComponentsTest.add(new JTextField("année"));
+        listComponentsTest.add(new JLabel("Date d'embauche"));
+        listComponentsTest.add(new JTextField("jour"));
+        listComponentsTest.add(new JComboBox(mois));
+        listComponentsTest.add(new JTextField("année"));
+        //ihm.addTo(paneGeneriqueTest, listComponentsTest);
+        ihm.disableAll(listComponentsTest);
+        labelMatriculeTest.setEnabled(true);
+        champMatriculeTest.setEnabled(true);
+
+        btnValiderEcranSupprimerTest.addActionListener(ihm);
+        btnAnnulerTest.addActionListener(ihm);
+        ihm.setButtonBounds(btnRechercherTest, 85);
+        ihm.setButtonBounds(btnReinitialiserTest, 190);
+        ihm.setButtonBounds(btnValiderEcranSupprimerTest, 295);
         ihm.setButtonBounds(btnAnnulerTest, 400);
 
         Assert.assertEquals("paneGenerique didn't match.", paneGeneriqueTest.toString(), field.get(ihm).toString());
