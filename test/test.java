c09bf442767854eb4a111cfb21248b8efcf64a18
Hadrien Tournès <hadrien.tournes@efrei.net>
2020-11-16 11:53:27 +0100

    made testDisplayModifierFinal() clearer

--- test/test.java
+++ test/test.java
@@ -18,34 +18,44 @@ public class test {
 
     @Test
     public void testDisplayModifierFinal() throws NoSuchFieldException, IllegalAccessException {
+        // Setup
         final Vue vue = new Vue();
-        final Field field = vue.getClass().getSuperclass().getDeclaredField("paneGenerique");
-        field.setAccessible(true);
+        final Field paneGenerique = vue.getClass().getSuperclass().getDeclaredField("paneGenerique");
+        paneGenerique.setAccessible(true);
+        paneGenerique.set(vue, new JPanel());
+        final Field btnRechercher = vue.getClass().getDeclaredField("btnRechercher");
+        final Field btnAnnuler = vue.getClass().getDeclaredField("btnAnnuler");
+        final Field btnReinitializer = vue.getClass().getDeclaredField("btnReinitialiser");
+        final Field btnValidateEcranModifier = vue.getClass().getDeclaredField("btnValiderEcranModifier");
+        btnAnnuler.setAccessible(true);
+        btnReinitializer.setAccessible(true);
+        btnRechercher.setAccessible(true);
+        btnValidateEcranModifier.setAccessible(true);
 
-        field.set(vue, new JPanel());
+        // When
         vue.displayModifierFinal();
 
-        Assert.assertEquals(4, ((JPanel) field.get(vue)).getComponentCount());
-        Component[] comps = ((JPanel) field.get(vue)).getComponents();
-        for (Component c : comps) {
-            Assert.assertEquals(JButton.class, c.getClass());
-            JButton button = (JButton) c;
-            if (button.getText() != "Réinitialiser") {
-                Assert.assertEquals(1, button.getActionListeners().length);
-                Assert.assertTrue(button.isEnabled());
-            } else {
-                Assert.assertEquals(0, button.getActionListeners().length);
-                Assert.assertFalse(button.isEnabled());
-            }
-        }
-        Assert.assertEquals(new Rectangle(85, 200, 100, 30), comps[0].getBounds());
-        Assert.assertEquals(new Rectangle(190, 200, 100, 30), comps[1].getBounds());
-        Assert.assertEquals(new Rectangle(295, 200, 100, 30), comps[2].getBounds());
-        Assert.assertEquals(new Rectangle(400, 200, 100, 30), comps[3].getBounds());
+        //Then
+        Assert.assertTrue("btnRechercher's text content is not Rechercher", ((JButton) btnRechercher.get(vue)).getText() == "Rechercher");
+        Assert.assertTrue("btnReinitializer's text content is not Réinitialiser", ((JButton) btnReinitializer.get(vue)).getText() == "Réinitialiser");
+        Assert.assertTrue("btnValidateEcranModifier's text content is not Valider", ((JButton) btnValidateEcranModifier.get(vue)).getText() == "Valider");
+        Assert.assertTrue("btnAnnuler's text content is not Annuler", ((JButton) btnAnnuler.get(vue)).getText() == "Annuler");
+
+        Assert.assertEquals("PaneGenerique size didn't match", ((JPanel) paneGenerique.get(vue)).getComponents().length, 4);
+
+        Assert.assertTrue("btnRechercher does not have a listener", ((JButton) btnRechercher.get(vue)).getActionListeners().length > 0);
+        Assert.assertTrue("btnAnnuler does not have a listener", ((JButton) btnAnnuler.get(vue)).getActionListeners().length > 0);
+        Assert.assertTrue("btnReinitialiser is enabled", !((JButton) btnReinitializer.get(vue)).isEnabled());
+        Assert.assertTrue("btnValiderEcranModifier does not have a listener", ((JButton) btnValidateEcranModifier.get(vue)).getActionListeners().length > 0);
+
+        Assert.assertTrue("btnRechercher wrong bounds", ((JButton) btnRechercher.get(vue)).getBounds().x == 85);
+        Assert.assertTrue("btnAnnuler wrong bounds", ((JButton) btnAnnuler.get(vue)).getBounds().x == 400);
+        Assert.assertTrue("btnReinitialiser wrong bounds", ((JButton) btnReinitializer.get(vue)).getBounds().x == 190);
+        Assert.assertTrue("btnValiderEcranModifier wrong bounds", ((JButton) btnValidateEcranModifier.get(vue)).getBounds().x == 295);
     }
 
     @Test
     public void testActionPerformed() {
-        
+
     }
 }
