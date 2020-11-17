5102a30d224be833698e2438bf8c8c16bcc822fc
driLegrand <adrien.demagny.com@gmail.com>
2020-11-17 19:45:52 +0100

    new unit test (getProgrammeurs, getResultSet and fermerRessources)

--- test/test.java
+++ test/test.java
@@ -1,18 +1,50 @@
 import modele.ActionsBDImpl;
 import org.junit.Assert;
 import org.junit.Test;
+import utils.Constantes;
+
+import javax.swing.*;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class test {
 
     public ActionsBDImpl conn = new ActionsBDImpl();
 
     @Test
-    public void stubbingReturnValue() {
-        Assert.assertTrue(true);
+    public void testFermerRessources() {
+        ActionsBDImpl tested = new ActionsBDImpl();
+        try{
+
+            tested.fermerRessources();
+            final Field field = tested.getClass().getDeclaredField("dbConn");
+            field.setAccessible(true);
+
+            Connection dbConn = (Connection) field.get(tested);
+
+            Assert.assertNull(dbConn);
+        } catch (NoSuchFieldException | IllegalAccessException exception) {
+            Logger.getLogger(ActionsBDImpl.class.getName()).log(Level.SEVERE, null, exception);
+        }
+
     }
 
     @Test
-    public void testAddProgrammer() {
-        Assert.assertNotEquals(0, conn.getProgrammeurs().size());
+    public void testGetProgrammeurs() {
+        ActionsBDImpl tested = new ActionsBDImpl();
+        Assert.assertTrue(tested.getProgrammeurs() instanceof ArrayList);
+    }
+
+    @Test
+    public void testGetResultSet() {
+        String query = Constantes.SELECT_ALL;
+        ActionsBDImpl tested = new ActionsBDImpl();
+        Assert.assertTrue(tested.getResultSet(query) instanceof ResultSet);
     }
 }
