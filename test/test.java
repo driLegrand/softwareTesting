f444caf3549a5692c7baf3ad147af031851bcb44
KasuraS <56578235+KasuraS@users.noreply.github.com>
2020-11-18 11:58:20 +0100

    Update test.java
    Add tests

--- test/test.java
+++ test/test.java
@@ -26,6 +26,60 @@ import java.util.ArrayList;
 public class test {
 
     public ActionsBDImpl conn = new ActionsBDImpl();
+ 
+    @Test
+    public void testAjouterProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(conn);
+        pstmt = conn.prepareStatement(Constantes.INSERT_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_MATR);
+        pstmt.setString(2, Constantes.TESTADDPROG_NOM);
+        pstmt.setString(3, Constantes.TESTADDPROG_PRENOM);
+        pstmt.setString(4, Constantes.TESTADDPROG_ADR);
+        pstmt.setString(5, Constantes.TESTADDPROG_PSEUDO);
+        pstmt.setString(6, Constantes.TESTADDPROG_RESP);
+        pstmt.setString(7, Constantes.TESTADDPROG_HOBBY);
+        pstmt.setString(8, Constantes.TESTADDPROG_DATNAISS);
+        pstmt.setString(9, Constantes.TESTADDPROG_DATEMP);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertEquals(1, isSuccessful);
+    }
+
+    @Test
+    public void testModifierProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(conn);
+        pstmt = conn.prepareStatement(Constantes.UPDATE_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_NOM);
+        pstmt.setString(2, Constantes.TESTADDPROG_PRENOM);
+        pstmt.setString(3, Constantes.TESTADDPROG_ADR);
+        pstmt.setString(4, Constantes.TESTUPDPROG_PSEUDO);
+        pstmt.setString(5, Constantes.TESTUPDPROG_RESP);
+        pstmt.setString(6, Constantes.TESTADDPROG_HOBBY);
+        pstmt.setString(7, Constantes.TESTADDPROG_DATNAISS);
+        pstmt.setString(8, Constantes.TESTUPDPROG_DATEMP);
+        pstmt.setString(9, Constantes.TESTADDPROG_MATR);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertNotEquals(0, isSuccessful);
+    }
+
+    @Test
+    public void testSupprimerProgrammeur() throws SQLException
+    {
+        Assert.assertNotNull(conn);
+        pstmt = conn.prepareStatement(Constantes.DELETE_UNIQUE);
+
+        pstmt.setString(1, Constantes.TESTADDPROG_MATR);
+
+        int isSuccessful = pstmt.executeUpdate();
+        Assert.assertNotEquals(0, isSuccessful);
+    }
+
+
 
     /**
      * Test to check closure of the Database
