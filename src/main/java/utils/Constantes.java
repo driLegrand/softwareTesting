aa9c2bf876ec68a6dfbe3ff24afcaf0eb422d5fb
driLegrand <adrien.demagny.com@gmail.com>
2020-12-09 09:47:41 +0100

    Local database

--- src/main/java/utils/Constantes.java
+++ src/main/java/utils/Constantes.java
@@ -1,13 +1,13 @@
 package utils;
 
 /**
- * Classe rassemblant toutes les constantes utiisées dans le programme
+ * Classe rassemblant toutes les constantes utilisées dans le programme
  */
 public class Constantes{
     public static final String OCCUPATION = "Programmeur";
-    public static final String URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7375737";
-    public static final String USER = "sql7375737";
-    public static final String MDP = "ICxuurfGJu";
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/TP2ST?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String MDP = "mysql";
     public static final String SELECT_ALL = "SELECT * FROM PROGRAMMEUR";
     public static final String SELECT_UNIQUE = "SELECT * FROM PROGRAMMEUR WHERE MATRICULE = ?";
     public static final String UPDATE_UNIQUE = "UPDATE PROGRAMMEUR SET NOM = ?, PRENOM = ?, ADRESSE = ?, PSEUDO = ?, RESPONSABLE = ?, HOBBY = ?, DATE_NAISS = ?, DATE_EMB = ? WHERE MATRICULE = ?";
